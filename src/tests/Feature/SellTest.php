bb5d6868edad87e9cadbf8e05498bc1522255d0c
nojino <nojinonoue@yahoo.co.jp>
2023-08-25 20:39:39 +0900

    phpunit test

--- src/tests/Feature/SellTest.php
+++ src/tests/Feature/SellTest.php
@@ -5,16 +5,70 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Item;
+use App\Models\Category;
+use Illuminate\Http\UploadedFile;
+
 
 class SellTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    public function test_sell()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/sell');
 
         $response->assertStatus(200);
     }
+
+    public function testExhibit()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->post('/exhibit', [
+            'user_id' => $user->id,
+            'name' => 'テスト商品',
+            'brand' => 'テストブランド',
+            'condition' => '新品、未使用',
+            'explanation' => 'テスト説明',
+            'price' => 1000,
+            'category' => ['カテゴリ1', 'カテゴリ2'],
+            'image' => UploadedFile::fake()->image('test.jpg')
+        ]);
+
+        $response->assertRedirect('/myPage');
+        $this->assertDatabaseHas('items', [
+            'name' => 'テスト商品',
+            'brand' => 'テストブランド',
+            'condition' => '新品、未使用',
+            'explanation' => 'テスト説明',
+            'price' => 1000,
+        ]);
+        $item = Item::first();
+        $this->assertDatabaseHas('categories', [
+            'item_id' => $item->id,
+            'category' => 'カテゴリ1'
+        ]);
+        $this->assertDatabaseHas('categories', [
+            'item_id' => $item->id,
+            'category' => 'カテゴリ2'
+        ]);
+    }
+
+    public function testWithdraw()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $item = Item::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->delete('/withdraw/'.$item->id);
+
+        $response->assertRedirect('/myPage');
+        $this->assertSoftDeleted($item);
+    }
+
 }
--- src/tests/Feature/WebhookTest.php
+++ src/tests/Feature/WebhookTest.php
@@ -5,16 +5,54 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Item;
+use App\Models\Purchase;
+use App\Models\Proceed;
+use Carbon\Carbon;
+use App\Mail\DepositedMail;
+use Illuminate\Support\Facades\Mail;
 
 class WebhookTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testHandlePayment()
+    {
+        Mail::fake();
+
+        $user = User::factory()->create();
+        $item = Item::factory()->create(['user_id' => $user->id]);
+        $purchase = Purchase::factory()->create([
+            'item_id' => $item->id,
+            'user_id' => $user->id,
+            'payment'=> 'test',
+            'postcode'=> '111-1111',
+            'address'=> 'test',
+            'payment_intent_id' => 'test_payment_intent_id'
+        ]);
+
+        $response = $this->postJson('/stripe/webhook', [
+            'type' => 'payment_intent.succeeded',
+            'data' => [
+                'object' => [
+                    'id' => 'test_payment_intent_id'
+                ]
+            ]
+        ]);
+
+        $response->assertOk();
+        $this->assertDatabaseHas('purchases', [
+            'id' => $purchase->id,
+            'deposited' => Carbon::now()
+        ]);
+        Mail::assertSent(DepositedMail::class, function ($mail) use ($user) {
+            return $mail->hasTo($user->email);
+        });
+        $this->assertDatabaseHas('proceeds', [
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+            'proceed' => $item->price
+        ]);
     }
 }
